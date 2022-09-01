cc65983ad5ce1312d741c1fddd19cc14cf49b6e0
piannode <142418867+piannode@users.noreply.github.com>
2022-09-01 20:26:38 +0000

    added copyright headers

--- ConfigureFlask - Chef/chef-repo/cookbooks/feedback cookbook/spec/unit/recipes/default_spec.rb
+++ ConfigureFlask - Chef/chef-repo/cookbooks/feedback cookbook/spec/unit/recipes/default_spec.rb
@@ -8,4 +8,13 @@ require 'spec_helper'
 
 describe 'feedback::default' do
   context 'When all attributes are default, on an unspecified platform' do
-    let(:c
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
---
+++ Deploy-As-Service-Using-AWS-EC2/app.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__,template_folder='templates')
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    features = [x for x in request.form.values()]
+    final_features = [np.array(features)]
+    column_names=['R&DSpend','Administration','MarketingSpend','State']
+    final_features=pd.
